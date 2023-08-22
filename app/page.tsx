d18b8f720dafb82e49c1770d87b541d5524eb30c
unknown <tarceandrei@gmail.com>
2023-08-22 13:41:48 +0300

    homepage design change

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,25 @@
 import Image from 'next/image'
 import background from '../assets/images/bg.png'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <main className='h-screen flex items-center'>
-      <div className='z-10 mb-60 mx-16 max-md:mx-4'>
-        <h1 className="text-7xl font-bold max-md:text-5xl leading-none">
+    <main className='h-[80vh] flex flex-col mt-28'>
+      <div className='z-10 mx-16 max-md:mx-4'>
+        <h1 className="text-7xl font-extrabold max-md:text-5xl leading-none">
           Rotaract Visio
         </h1>
         <span className='font-normal text-5xl max-md:text-4xl'>
           Cluj-Napoca
         </span>
+      </div>
+      <div className='z-10 mx-16 max-md:mx-4 mt-20 max-md:mt-24 text-4xl flex flex-col gap-2'>
+        <span>
+          Vrei să te implici?
+        </span>
+        <Button className='w-44 text-lg bg-rotaract-cranberry text-white hover:bg-[#020817BB]'>
+          Contactează-ne
+        </Button>
       </div>
       <Image src={background} alt="background" quality={100} className='fixed object-cover h-full top-0 z-0 brightness-75' />
     </main>
