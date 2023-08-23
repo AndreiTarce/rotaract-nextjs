a52846d63aec2a20e2947ec58897a1a41407399a
unknown <tarceandrei@gmail.com>
2023-08-23 17:42:19 +0300

    add project cards structure

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,18 @@
+import ProjectCard from "@/components/ui/project/ProjectCard";
+
 export default function Projects() {
+
+
     return (
-        <>Projects</>
+        <main className="mt-28 mx-16 max-md:mx-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+        </main>
     )
 };
