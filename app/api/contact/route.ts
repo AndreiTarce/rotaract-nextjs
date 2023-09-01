47ca196ceb745a452cff4f451cfa02d522c106c9
unknown <tarceandrei@gmail.com>
2023-09-02 00:02:58 +0300

    implement rate limiting for contact api

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,12 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { Resend } from 'resend';
 import { ContactFormSchema } from '@/components/ui/contact/ContactForm';
+import rateLimit from '../utils/rate-limiter';
 
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(request: NextRequest) {
+const limiter = rateLimit({ interval: 60 * 1000 * 60 * 24, uniqueTokenPerInterval: 100 })
+
+export async function POST(request: NextRequest, response: NextResponse) {
     try {
+        await limiter.check(response, 10, 'CACHE_TOKEN');
         const body: ContactFormSchema = await request.json();
         const data = await resend.emails.send({
             from: 'contact@rotaractvisio.com',
@@ -14,9 +18,8 @@ export async function POST(request: NextRequest) {
             subject: body.subject,
             html: body.message
         });
-
-        return NextResponse.json(data);
+        return NextResponse.json(data, { status: 200 });
     } catch (error) {
-        return NextResponse.json({ error });
+        return NextResponse.json({ error: 'Too many requests.' }, { status: 429 });
     }
 }
