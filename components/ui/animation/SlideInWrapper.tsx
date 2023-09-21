1a0e82ca83ee55e57ba9e48d8c2a0cca1eb021fb
unknown <tarceandrei@gmail.com>
2023-09-21 19:23:10 +0300

    fix choppiness

--- components/ui/animation/SlideInWrapper.tsx
+++ components/ui/animation/SlideInWrapper.tsx
@@ -4,12 +4,12 @@ import { motion } from 'framer-motion'
 export default function SlideInWrapper(props: { children: React.ReactNode }) {
     return (
         <motion.div
-            transition={{ duration: 1 }}
+            transition={{ duration: 0.5 }}
             initial={{ opacity: 0, translateX: -150 }}
             whileInView={{
                 opacity: 1,
                 translateX: 0,
-                transition: { delay: 0.3 },
+                transition: { ease: 'easeOut' },
             }}
         >
             {props.children}
