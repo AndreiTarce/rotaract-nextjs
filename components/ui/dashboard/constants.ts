66c7c44aff77c923ac9c54f6a0a9b535cabd3f88
AndreiTarce <tarceandrei@gmail.com>
2024-01-18 14:19:15 +0200

    add sedinta board type

--- components/ui/dashboard/constants.ts
+++ components/ui/dashboard/constants.ts
@@ -2,4 +2,5 @@ export const MEETING_TYPES = [
     { name: 'Sedinta Club', id: 'sedinta_club' },
     { name: 'Sedinta Vedere de Craciun', id: 'sedinta_vederedecraciun' },
     { name: 'Sedinta Cauza Panticeu', id: 'sedinta_panticeu' },
+    { name: 'Sedinta Board', id: 'sedinta_board' },
 ]
