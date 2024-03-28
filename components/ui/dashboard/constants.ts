caf72a7525013da8cd6e5027595c8e6892f393d1
AndreiTarce <tarceandrei@gmail.com>
2024-03-28 16:42:50 +0200

    add catrafusale meeting

--- components/ui/dashboard/constants.ts
+++ components/ui/dashboard/constants.ts
@@ -3,4 +3,5 @@ export const MEETING_TYPES = [
     { name: 'Sedinta Vedere de Craciun', id: 'sedinta_vederedecraciun' },
     { name: 'Sedinta Cauza Panticeu', id: 'sedinta_panticeu' },
     { name: 'Sedinta Board', id: 'sedinta_board' },
+    { name: 'Sedinta CATRAFUSALE', id: 'sedinta_catrafusale' },
 ]
