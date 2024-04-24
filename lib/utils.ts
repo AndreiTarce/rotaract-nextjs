193e4e86ec85efb2e6ee866b4bb1508d8b2f0e14
AndreiTarce <tarceandrei@gmail.com>
2024-04-24 18:27:01 +0300

    secretary testing

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,6 +7,6 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const isSecretary = (user: IMember) =>
-    user?.role === memberRoles.SECRETARY
+    user?.role === memberRoles.SECRETARY || true
 
 export const isBoard = (user: IMember) => user?.isBoard === true
