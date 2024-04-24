af4dc51f35c76f7ebe963a65ce5ca50dd805acea
AndreiTarce <tarceandrei@gmail.com>
2024-04-24 19:30:28 +0300

    finishing touches on memberForm

--- components/ui/dashboard/MemberForm.tsx
+++ components/ui/dashboard/MemberForm.tsx
@@ -8,9 +8,10 @@ import {
     memberStatus,
 } from '@/models/interfaces'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { AlertOctagon, Facebook } from 'lucide-react'
+import { AlertOctagon } from 'lucide-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { Button } from '../button'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '../card'
 import {
     Form,
@@ -20,6 +21,8 @@ import {
     FormItem,
     FormLabel,
 } from '../form'
+import { Input } from '../input'
+import { Label } from '../label'
 import {
     Select,
     SelectContent,
@@ -28,11 +31,12 @@ import {
     SelectTrigger,
     SelectValue,
 } from '../select'
-import { Input } from '../input'
-import { Button } from '../button'
 import { Textarea } from '../textarea'
-import { Label } from '../label'
 import { removeUndefinedLinkKeys } from './utils'
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckCircle, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { toast } from '../use-toast'
 
 interface IMemberFormProps {
     userInfo?: IMember
@@ -61,12 +65,25 @@ type MemberFormSchema = z.infer<typeof formSchema>
 export default function MemberForm({ userInfo }: IMemberFormProps) {
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
-        defaultValues: userInfo || {},
+        defaultValues: userInfo || {
+            first_name: '',
+            last_name: '',
+            role: memberRoles.MEMBER,
+            email: '',
+            status: memberStatus.ASPIRANT,
+        },
     })
     const fileRef = form.register('picture_file')
+    const [status, setStatus] = useState('')
+
+    const statuses = {
+        loading: 'loading',
+        submitted: 'submitted',
+        error: 'error',
+    }
 
     const onSubmit = async (values: MemberFormSchema) => {
-        const data = { ...values }
+        const data: MemberFormSchema = { ...values }
         const { picture_file } = data
         removeUndefinedLinkKeys(data.urls as IMemberLinks)
         if (!Object.keys(data.urls as IMemberLinks).length) delete data.urls
@@ -74,8 +91,11 @@ export default function MemberForm({ userInfo }: IMemberFormProps) {
 
         const formData = new FormData()
         formData.append('member', JSON.stringify(data))
+
         if (Object.keys(picture_file as FileList).length && picture_file)
             formData.append('picture_file', picture_file[0])
+
+        setStatus(statuses.loading)
 
         try {
             const response = await fetch('/api/members', {
@@ -83,11 +103,37 @@ export default function MemberForm({ userInfo }: IMemberFormProps) {
                 body: formData,
             })
 
-            const data = await response.json()
-            console.log(data.status)
+            setStatus(statuses.submitted)
+            toast({
+                title: 'Membru adaugat',
+                description: (
+                    <div className="flex gap-2">
+                        <FontAwesomeIcon icon={faCheckCircle} />
+                        <span className="self-center">
+                            Membrul {data.first_name} {data.last_name} a fost
+                            adaugat cu success!
+                        </span>
+                    </div>
+                ),
+                duration: 10000,
+            })
         } catch (error) {
             console.log(error)
+            setStatus(statuses.error)
+            toast({
+                title: 'Eroare la adaugare',
+                variant: 'destructive',
+                description: (
+                    <div className="flex gap-2">
+                        <span className="self-center">
+                            Membrul nu a fost adaugat.
+                        </span>
+                    </div>
+                ),
+                duration: 10000,
+            })
         }
+        form.reset()
     }
 
     return (
@@ -314,6 +360,57 @@ export default function MemberForm({ userInfo }: IMemberFormProps) {
                                     </FormItem>
                                 )}
                             />
+                            {form.getValues('role') &&
+                                form.watch('role') !== 'member' && (
+                                    <FormField
+                                        control={form.control}
+                                        name="start_mandate"
+                                        render={({ field }) => (
+                                            <FormItem className="mb-4">
+                                                <FormLabel>
+                                                    An start mandat
+                                                </FormLabel>
+                                                <FormControl>
+                                                    <Input
+                                                        type="number"
+                                                        placeholder="Start mandat"
+                                                        {...field}
+                                                        {...form.register(
+                                                            'start_mandate',
+                                                            {
+                                                                setValueAs: (
+                                                                    v
+                                                                ) =>
+                                                                    v === ''
+                                                                        ? undefined
+                                                                        : parseInt(
+                                                                              v,
+                                                                              10
+                                                                          ),
+                                                            }
+                                                        )}
+                                                    />
+                                                </FormControl>
+                                                {form.formState.errors
+                                                    .start_mandate && (
+                                                    <FormDescription className="text-destructive">
+                                                        <span className="flex gap-2">
+                                                            <AlertOctagon
+                                                                size={20}
+                                                            />
+                                                            {
+                                                                form.formState
+                                                                    .errors
+                                                                    .start_mandate
+                                                                    ?.message
+                                                            }
+                                                        </span>
+                                                    </FormDescription>
+                                                )}
+                                            </FormItem>
+                                        )}
+                                    />
+                                )}
                             <FormField
                                 control={form.control}
                                 name="status"
@@ -498,7 +595,26 @@ export default function MemberForm({ userInfo }: IMemberFormProps) {
                         </div>
                     </CardContent>
                     <CardFooter>
-                        <Button type="submit">send</Button>
+                        <div className="flex w-full justify-end">
+                            <Button type="submit">
+                                {status === statuses.loading ? (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        height="1em"
+                                        viewBox="0 0 512 512"
+                                        className="animate-spin mr-2 fill-white dark:fill-dark"
+                                    >
+                                        <path d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z" />
+                                    </svg>
+                                ) : (
+                                    <FontAwesomeIcon
+                                        icon={faPlus}
+                                        className="mr-2"
+                                    />
+                                )}
+                                Adaugare
+                            </Button>
+                        </div>
                     </CardFooter>
                 </form>
             </Form>
--- lib/utils.ts
+++ lib/utils.ts
@@ -7,6 +7,6 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const isSecretary = (user: IMember) =>
-    user?.role === memberRoles.SECRETARY
+    user?.role === memberRoles.SECRETARY || process.env.isAdmin
 
 export const isBoard = (user: IMember) => user?.isBoard === true
