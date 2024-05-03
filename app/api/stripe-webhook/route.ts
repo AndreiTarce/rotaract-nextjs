f6e50b9a8a7b300453e0eea746b4e1f127f69520
Andrei Tarce <andrei.tarce@accesa.eu>
2024-05-03 21:45:21 +0300

    update webhook endpoint

--- app/api/stripe-webhook/route.ts
+++ app/api/stripe-webhook/route.ts
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 const endpointSecret =
     'whsec_e42a0ea000c076f5b92dafb1574bc98dc44f9d2341105289cc670da525104722'
 
-export async function POST(request: Request, response: NextResponse) {
+export async function POST(request: NextRequest, response: NextResponse) {
     const body = await request.text()
 
     const sig = request.headers.get('stripe-signature')
