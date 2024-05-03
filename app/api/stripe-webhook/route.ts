addd098b649c979a6278f8d33fc8cc27c43e03fb
Andrei Tarce <andrei.tarce@accesa.eu>
2024-05-03 21:05:52 +0300

    add test webhook endpoint

---
+++ app/api/stripe-webhook/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from 'next/server'
+import Stripe from 'stripe'
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
+
+const endpointSecret =
+    'whsec_e42a0ea000c076f5b92dafb1574bc98dc44f9d2341105289cc670da525104722'
+
+export async function POST(request: NextRequest, response: NextResponse) {
+    const body = await request.json()
+
+    const sig = request.headers.get('stripe-signature')
+
+    let event
+
+    try {
+        event = stripe.webhooks.constructEvent(
+            body,
+            sig as string,
+            endpointSecret
+        )
+    } catch (err) {
+        return NextResponse.json(
+            { error: `Webhook Error: ${err}` },
+            { status: 400 }
+        )
+    }
+
+    // Handle the event
+    console.log(`Unhandled event type ${event.type}`)
+
+    // Return a 200 response to acknowledge receipt of the event
+    return NextResponse.json({ status: 200 })
+}
