29f56db61d94ec15b4082e481af6bb32e8f31b00
AndreiTarce <tarceandrei@gmail.com>
2024-07-12 20:30:00 +0300

    redeploy without projectsList

--- components/ui/project/Projects.tsx
+++ components/ui/project/Projects.tsx
@@ -1,10 +1,10 @@
 import { IProject } from '@/interfaces/IProject';
 import { getProjects } from '@/lib/entityService';
-import ProjectCard from './ProjectCard';
 
 export default async function ProjectsList() {
     const projects: IProject[] = await getProjects();
-    return projects.map((project: IProject, index: number) => (
-        <ProjectCard key={index} {...project} />
-    ));
+    // return projects.map((project: IProject, index: number) => (
+    //     <ProjectCard key={index} {...project} />
+    // ));
+    return <>deploy</>;
 }
