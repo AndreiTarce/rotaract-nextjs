99737368447f6931bb1e0ca023eae60cb8e6fd83
AndreiTarce <tarceandrei@gmail.com>
2024-07-23 17:24:59 +0300

    add null check

--- components/ui/members/Members.tsx
+++ components/ui/members/Members.tsx
@@ -4,11 +4,11 @@ import MemberCard from './MemberCard';
 
 export default async function MembersList() {
     const members: MemberDto[] = await getMembers();
-    const activeMembers = members.filter(
+    const activeMembers = members?.filter(
         (member: MemberDto) =>
             member.status === 'activ' && member.role === 'member'
     );
-    return activeMembers.map((member: MemberDto, index: number) => (
+    return activeMembers?.map((member: MemberDto, index: number) => (
         <MemberCard key={index} {...member} />
     ));
 }
