57665c7d7903fd33b0af39820b943940f3105d36
AndreiTarce <tarceandrei@gmail.com>
2024-07-23 17:27:55 +0300

    delete type assignment

--- components/ui/members/Members.tsx
+++ components/ui/members/Members.tsx
@@ -3,12 +3,12 @@ import { getMembers } from '@/lib/entityService';
 import MemberCard from './MemberCard';
 
 export default async function MembersList() {
-    const members: MemberDto[] = await getMembers();
-    const activeMembers = members?.filter(
+    const members = await getMembers();
+    const activeMembers = members.filter(
         (member: MemberDto) =>
             member.status === 'activ' && member.role === 'member'
     );
-    return activeMembers?.map((member: MemberDto, index: number) => (
+    return activeMembers.map((member: MemberDto, index: number) => (
         <MemberCard key={index} {...member} />
     ));
 }
