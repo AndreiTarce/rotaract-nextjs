028c2bc322e7fa270430b4e765948272488b346b
AndreiTarce <tarceandrei@gmail.com>
2024-08-01 18:16:56 +0300

    add connectMongoDb to usecases

--- use-cases/members/getBoardMembers.ts
+++ use-cases/members/getBoardMembers.ts
@@ -1,9 +1,12 @@
 import { MemberInteractor } from '@/interactors/memberInteractor';
+import connectMongoDB from '@/lib/mongodb';
 import { MemberRepository } from '@/repositories/memberRepository';
 
 const memberIntarctor = new MemberInteractor(new MemberRepository());
 
 export async function getBoardMembers() {
+    await connectMongoDB();
+
     const boardMembers = await memberIntarctor.getMembersWithQuery({
         isBoard: true,
     });
--- use-cases/members/getOrderedPastPresidents.ts
+++ use-cases/members/getOrderedPastPresidents.ts
@@ -1,9 +1,12 @@
 import { MemberInteractor } from '@/interactors/memberInteractor';
+import connectMongoDB from '@/lib/mongodb';
 import { MemberRepository } from '@/repositories/memberRepository';
 
 const memberInteractor = new MemberInteractor(new MemberRepository());
 
 export async function getOrderedPastPresidents() {
+    await connectMongoDB();
+
     const pastPresidents = await memberInteractor.getMembersWithQuery({
         role: 'past president',
     });
