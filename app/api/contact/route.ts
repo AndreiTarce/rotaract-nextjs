79b3e56dd384f4a9c68ac19855c5ff144bf48089
AndreiTarce <tarceandrei@gmail.com>
2024-08-05 16:38:13 +0300

    revert back to old contact implementation

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,6 +1,7 @@
 import { ContactFormSchema } from '@/components/ui/contact/ContactForm';
-import { sendContactEmail } from '@/use-cases/contact/sendContactEmail';
+import { ROTARACT_VISIO_CONTACT_EMAIL } from '@/lib/constants';
 import { NextRequest, NextResponse } from 'next/server';
+import { Resend } from 'resend';
 import rateLimit from '../utils/rate-limiter';
 
 const limiter = rateLimit({
@@ -8,13 +9,25 @@ const limiter = rateLimit({
     uniqueTokenPerInterval: 100,
 });
 
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export async function POST(request: NextRequest, response: NextResponse) {
     try {
         await limiter.check(response, 10, 'CACHE_TOKEN');
         const body: ContactFormSchema = await request.json();
         const bodyString = `NUME: ${body.first_name} ${body.last_name}\nEMAIL: ${body.email}\nMESSAGE:${body.message}`;
-        sendContactEmail(body.subject, bodyString);
-        return new NextResponse(null, { status: 200 });
+        const data = await resend.emails.send({
+            from: ROTARACT_VISIO_CONTACT_EMAIL,
+            to: [
+                'tarceandrei@gmail.com',
+                'rotaractvisiocluj@gmail.com',
+                'gabrielamusteata28@gmail.com',
+                'alexmuresan.dacian@gmail.com',
+            ],
+            subject: body.subject,
+            html: bodyString,
+        });
+        return NextResponse.json(data, { status: 200 });
     } catch (error) {
         return NextResponse.json(
             { error: 'Too many requests.' },
