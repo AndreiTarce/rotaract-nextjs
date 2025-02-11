12886d8b5390d4485ec3615f58acd8f4331839f8
AndreiTarce <tarceandrei@gmail.com>
2025-02-11 18:48:19 +0200

    move scroll progress under navbar

--- components/ui/scroll-progress.tsx
+++ components/ui/scroll-progress.tsx
@@ -18,7 +18,7 @@ export function ScrollProgress({ className }: ScrollProgressProps) {
     return (
         <motion.div
             className={cn(
-                'fixed inset-x-0 top-0 z-[1000] h-1 origin-left bg-gradient-to-r from-rotaract-cranberry to-rose-500',
+                'fixed inset-x-0 z-[1000] h-1 origin-left bg-gradient-to-r from-rotaract-cranberry to-rose-500',
                 className
             )}
             style={{
