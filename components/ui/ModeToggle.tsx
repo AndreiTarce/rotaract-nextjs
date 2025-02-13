4e76472f97367bad85b870a228b8bfa63c3b02b0
AndreiTarce <tarceandrei@gmail.com>
2025-02-13 15:16:27 +0200

    add pointer cursor on button hover

--- components/ui/ModeToggle.tsx
+++ components/ui/ModeToggle.tsx
@@ -1,26 +1,26 @@
-'use client'
+'use client';
 
-import { Moon, Sun } from 'lucide-react'
-import { useTheme } from 'next-themes'
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes';
 
-import { Button } from '@/components/ui/button'
+import { Button } from '@/components/ui/button';
 
 export function ModeToggle() {
-    const { setTheme, theme } = useTheme()
+    const { setTheme, theme } = useTheme();
 
     const toggleTheme = () => {
         if (theme === 'dark') {
-            setTheme('light')
+            setTheme('light');
         } else {
-            setTheme('dark')
+            setTheme('dark');
         }
-    }
+    };
 
     return (
         <Button variant="outline" size="icon" onClick={toggleTheme}>
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
             <span className="sr-only">Toggle theme</span>
         </Button>
-    )
+    );
 }
